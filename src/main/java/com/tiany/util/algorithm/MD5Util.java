2c4bb8f580756d813ca6794da1ba4ff2d2c6d750
tiany <tianyao_job@163.com>
2018-05-19 20:00:24 +0800

    update by tianyao

--- src/main/java/com/tiany/util/algorithm/MD5Util.java
+++ src/main/java/com/tiany/util/algorithm/MD5Util.java
@@ -14,10 +14,16 @@ public class MD5Util {
 	 * @throws NoSuchAlgorithmException
 	 * @throws IllegalArgumentException
 	 */
-	public static String md5(String originalString)
-			throws UnsupportedEncodingException, NoSuchAlgorithmException, IllegalArgumentException {
-		byte[] signedOriginalStringBytes = MessageDigest.getInstance("MD5").digest(originalString.getBytes("UTF-8"));
-		String signedOriginalString = new HexBinaryAdapter().marshal(signedOriginalStringBytes);
+	public static String md5(String originalString) {
+		String signedOriginalString = null;
+		try {
+			byte[] signedOriginalStringBytes = MessageDigest.getInstance("MD5").digest(originalString.getBytes("UTF-8"));
+			signedOriginalString = new HexBinaryAdapter().marshal(signedOriginalStringBytes);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
 		return signedOriginalString;
 	}
 }
