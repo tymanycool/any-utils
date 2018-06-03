64c043e6b5638e77a43e492aaa0fe208d30108d6
tiany <tianyao_job@163.com>
2018-06-03 16:32:05 +0800

    update by tianyao

---
+++ src/main/java/com/tiany/util/CollectionUtil.java
@@ -0,0 +1,72 @@
+package com.tiany.util;
+
+import com.tiany.inf.Condition;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionUtil {
+    /**
+     * 数组arr中是否包含ch
+     * @param arr
+     * @param ch
+     * @return
+     */
+    public static boolean contains(char[] arr,char ch){
+        for(char c :arr){
+            if (ch == c){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    /**
+     * list中是否包含str(忽略大小写)
+     * @param list
+     * @param str
+     * @return
+     */
+    public static boolean containsIgnoreCase(List<String> list,String str){
+        for(String s:list){
+            if(s.toUpperCase().equals(str.toUpperCase())){
+                return true;
+            }
+        }
+        return false;
+    }
+    /**
+     * 按照条件分割list
+     *
+     * @param list
+     * @param condition
+     * @param <T>
+     * @return
+     */
+    public static <T> List<List<T>> split(List<T> list, Condition condition) {
+        List<List<T>> ret = new ArrayList<>();
+        List<T> sublist = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            if (i + 1 == list.size()) {
+                if (condition.matches(list.get(i), null)) {
+                    ret.add(sublist);
+                    sublist = new ArrayList<>();
+                    continue;
+                }
+            } else {
+                if (condition.matches(list.get(i), list.get(i + 1))) {
+                    ret.add(sublist);
+                    sublist = new ArrayList<>();
+                    continue;
+                }
+            }
+            sublist.add(list.get(i));
+        }
+        if(sublist.size()>0){
+            ret.add(sublist);
+        }
+        return ret;
+
+    }
+}
--- src/main/java/com/tiany/util/MapUtil.java
+++ src/main/java/com/tiany/util/MapUtil.java
@@ -5,12 +5,46 @@ import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.Method;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * Map工具类
  */
 public abstract class MapUtil {
+    /**
+     * map中是否包含key(忽略大小写)
+     * @param map
+     * @param key
+     * @return
+     */
+    public static Object getIgnoreCase(Map<String,Object> map,String key){
+        Iterator<Map.Entry<String, Object>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()){
+            Map.Entry<String, Object> next = iterator.next();
+            if(Objects.toString(next.getKey()).toUpperCase().equals(Objects.toString(key).toUpperCase())){
+                return next.getValue();
+            };
+        }
+        return null;
+    }
+    /**
+     * map中是否包含key(忽略大小写)
+     * @param map
+     * @param key
+     * @return
+     */
+    public static boolean containsKeyIgnoreCase(Map<String,Object> map,String key){
+        Iterator<String> iterator = map.keySet().iterator();
+        while (iterator.hasNext()){
+            String next = iterator.next();
+            if(Objects.toString(next).toUpperCase().equals(key.toUpperCase())){
+                return true;
+            };
+        }
+        return false;
+    }
 
     public static Map<String, Object> transBean2Map(Object obj) {
 
