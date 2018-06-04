ff134b713af6f557a79b83879fd9c80dca0d53b2
tiany <tianyao_job@163.com>
2018-06-04 11:38:24 +0800

    update by tianyao

--- src/test/java/com/tiany/util/format/JsonUtilTest.java
+++ src/test/java/com/tiany/util/format/JsonUtilTest.java
@@ -4,18 +4,44 @@ import org.junit.Test;
 
 import java.util.HashMap;
 
+class User{
+    String lastName;
+    int age;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
 public class JsonUtilTest {
     @Test
     public void mapToJson() throws Exception {
 
         HashMap<Object, Object> map = new HashMap<>();
-        map.put("username","tianyao");
+        map.put("userName","tianyao");
         map.put("age", 23);
 
         HashMap<Object, Object> map2 = new HashMap<>();
         map2.put("username","tianyao");
         map2.put("age", 23);
         map.put("map2",map2);
+
+        User user = new User();
+        user.setAge(24);
+        user.setLastName("TIANYAO");
+        map.put("user",user);
 
         String s = JsonUtil.obj2Json(map);
         System.out.println(s);
